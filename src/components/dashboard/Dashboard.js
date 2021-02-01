bc42ec6a5b68a0513eccc8de95c9b9205c0876a4
Wiza Kaluba <kaluba.w@gmail.com>
2021-02-01 16:04:43 +0000

    Added stubs for dashboard section

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import './Dashboard.scss';
+
+function Dashboard(props) {
+    return (
+        <div className="dashboard">
+            {
+            // TO-DO: Graphs for viewing trading prices
+            // TO-DO: Summary section for showing equity value}
+            }
+        </div>
+    )
+}
+
+export default Dashboard;
