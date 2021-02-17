f9e88e07484689a728b639b80e02c922d4cf407c
Wiza Kaluba <kaluba.w@gmail.com>
2021-02-17 18:50:48 +0000

    Added Nacbar

---
+++ src/components/header/navbar/Navbar.js
@@ -0,0 +1,12 @@
+function Navbar(props) {
+    return (
+        <nav className="navbar">
+            <a href="#">One</a>
+            <a href="#">Two</a>
+            <a href="#">Three</a>
+            <a href="#">Four</a>
+        </nav>
+    )
+}
+
+export default Navbar;
